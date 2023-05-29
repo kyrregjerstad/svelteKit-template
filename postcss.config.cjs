507aaa3fb5ef54b75b83d59f7be1e5c07f478cc7
Kyrre Gjerstad <kyrregjerstad@gmail.com>
2023-05-29 12:44:25 +0200

    lint and add postcss

---
+++ postcss.config.cjs
@@ -0,0 +1,18 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const postcssPresetEnv = require("postcss-preset-env");
+
+/** @type {import('postcss-load-config').Config} */
+const config = {
+	plugins: [
+		postcssPresetEnv({
+			stage: 3,
+			features: {
+				"nesting-rules": true,
+				"custom-media-queries": true,
+				"media-query-ranges": true
+			}
+		})
+	]
+};
+
+module.exports = config;
